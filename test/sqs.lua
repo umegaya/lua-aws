1da492c9e04fc2e6432c4fccb27b8d6b738137f9
Takehiro Iyatomi <iyatomi@gmail.com>
2017-12-26 10:42:21 +0900

    test behavior of map shape

--- test/sqs.lua
+++ test/sqs.lua
@@ -4,6 +4,7 @@ local aws = AWS.new({
 	accessKeyId = os.getenv('AWS_ACCESS_KEY'),
 	secretAccessKey = os.getenv('AWS_SECRET_KEY'),
 })
+
 local dump_res = helper.dump_res
 local ok,r
 ok,r = aws.SQS:api_by_version('2012-11-05'):createQueue({
@@ -37,13 +38,23 @@ local params = {
 		{
 			Id = "msg1",
 			MessageBody = "test msg 1",
+			MessageAttributes = {
+				test_attribute_name_1 = {
+					StringValue = "test_attribute_value_1_1",
+					DataType = "String",
+				},
+			},
 		},
 		{
 			Id = "msg2",
 			MessageBody = "test msg 2",
 			MessageAttributes = {
 				test_attribute_name_1 = {
-					StringValue = "test_attribute_value_1",
+					StringValue = "test_attribute_value_1_2",
+					DataType = "String",
+				},
+				test_attribute_name_2 = {
+					StringValue = "test_attribute_value_2_2",
 					DataType = "String",
 				},
 			},
@@ -57,11 +68,17 @@ dump_res('sendbatch', r)
 
 ok,r = aws.SQS:api_by_version('2012-11-05'):receiveMessage({
 	QueueUrl = QueueUrl,
-	Foo = Bar, -- un-ruled parameter test
+	Foo = Bar, -- un-ruled parameter test,
+	MaxNumberOfMessages = 10,
+	MessageAttributeNames={"All"},
 })
 assert(ok, r)
 dump_res('recv2', r)
-
+local msg2 = r.value.ReceiveMessageResponse.value.ReceiveMessageResult.value.Message[2]
+assert(msg2.value.MessageAttribute[1].value.Name.value == 'test_attribute_name_1')
+assert(msg2.value.MessageAttribute[1].value.Value.value.StringValue.value == 'test_attribute_value_1_2')
+assert(msg2.value.MessageAttribute[2].value.Name.value == 'test_attribute_name_2')
+assert(msg2.value.MessageAttribute[2].value.Value.value.StringValue.value == 'test_attribute_value_2_2')
 
 ok,r = aws.SQS:api_by_version('2012-11-05'):deleteQueue({
 	QueueUrl = QueueUrl,
