57d4fc757d526f3e6d4e3a4208406c276efb8ba2
Takehiro Iyatomi <iyatomi@gmail.com>
2019-01-31 15:01:36 +0900

    detect moto_server spinup by using curl, and send moto_server output to /dev/null, because wait for spinning up produces verbose warnings

--- test/tools/run.sh
+++ test/tools/run.sh
@@ -27,14 +27,22 @@ if [ ! -z $2 ]; then
 		# inject service name which use with mock
 		SERVICE=`basename $1 .lua`
 		# run moto_server with service name
-		moto_server $SERVICE &
+		moto_server $SERVICE > /dev/null 2>&1 &
 		PID=$!
 		if [ -z $PID ]; then
 			echo "cannot get pid"
 			exit 1
 		fi
-		echo "running mock server at $PID"
-		sleep 2
+		printf "spinning up mock server at $PID"
+		while : ; do
+			printf "."
+			curl -s http://127.0.0.1:5000 2>&1 > /dev/null
+			if [ $? -eq 0 ]; then
+				break
+			fi
+			sleep 1
+		done
+		echo "start"
 		# run test with using mock
 		$LUA $1 http://127.0.0.1:5000
 		RESULT=$?
